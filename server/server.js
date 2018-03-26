3c41278b22f871e350f796622b5d5ca2e21d5d99
j-ornelas <johnnyoh1988@gmail.com>
2018-03-26 13:53:32 -0700

    Removes new relic for added load performance

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const newrelic = require('newrelic');
 const app = require('./app.js');
-app.locals.newrelic = newrelic;
+
+// const newrelic = require('newrelic');
+// app.locals.newrelic = newrelic;
 
 let port = process.env.PORT || 3002;
 app.listen(port, () => console.log('Express listening on port ', port));
