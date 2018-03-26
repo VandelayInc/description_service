9a2e61170220dbbea39e94318aa0402a640e7e19
j-ornelas <johnnyoh1988@gmail.com>
2018-03-26 13:52:22 -0700

    Tried to set up SSR and failed

--- server/api-routerSQL.js
+++ server/api-routerSQL.js
@@ -1,34 +1,56 @@
 const express = require('express');
 //pick databases here, too
-// const db = require('../db/model.js');
-const db = require('../db/modelMySQL.js');
+const db = require('../db/model.js');
+// const db = require('../db/modelMySQL.js');
 const router = express.Router();
 const mongoose = require('mongoose');
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import AppDescription from '../client/src/js/AppDescription.jsx';
 
+const redis = require('redis');
+
+// create a new redis client and connect to our local redis instance
+const client = redis.createClient();
+
+// if an error occurs, print it to the console
+client.on('error', function (err) {
+    console.log("Error " + err);
+});
+
+
+
+
 router
 // vVv uncomment this to use Mongo vVv
-  // .get('/:roomid/description', (req, res) => {
-  //   console.log('reqparams', req.params.roomid)
-  //   db.DescriptionModel.find({_id:req.params.roomid})
-  //   .then((data) => {
-  //     console.log('dataSQL ROUTER', data)
-  //     res.json(data[0])
-  //   })
-  // })
+.get('/:roomid/description', (req, res) => {
+  var id = req.params.roomid;
+  client.get(id, function(err, result){
+    if (result){
+      res.header('Access-Control-Allow-Origin', '*')
+      .json(JSON.parse(result))
+    } else {
+      db.DescriptionModel.find({_id:id})
+      .then((data) => {
+        var dataString = JSON.stringify(data[0])
+        client.setex(id, 60, dataString)
+        res.header('Access-Control-Allow-Origin', '*')
+        .json(data[0])
+      })
+    }
+  })
+})
 
 // vVv uncomment this to use SQL vVv
-  .get('/:roomid/description', (req, res) => {
-    let id = req.params.roomid
-    console.log('ROOM ID', id)
-    db.sequelize.query(`SELECT * FROM giantdata WHERE id = ${id}`)
-      .then(data => {
-        console.log(data[0])
-        res.send(data[0][0])
-      })
-  })
+  // .get('/:roomid/description', (req, res) => {
+  //   let id = req.params.roomid
+  //   console.log('ROOM ID', id)
+  //   db.sequelize.query(`SELECT * FROM giantdata WHERE id = ${id}`)
+  //     .then(data => {
+  //       console.log(data[0])
+  //       res.send(data[0][0])
+  //     })
+  // })
 
   // .get('/:roomid/description', (req, res) => {
   //   let id = req.params.roomid
@@ -43,30 +65,11 @@ router
 
 
 // router
-//   .route('/:roomid/description/ssr')
-//   .get((req, res, next) => {
-//     db.findOne(+req.params.roomid)
-//       .then(desc => res.send(renderToString(<AppDescription roomId={req.params.roomid} description={desc}/>)))
-//       .catch(err => {
-//         console.log('Error retrieving description for room ', req.params.roomid, ' from database');
-//         res.sendStatus(404);
-//       });
-//   })
-//   .options((req, res) => {
-//     res.sendStatus(200);
-//   });
-
-module.exports = router;
-
-
-
-//old route
-  // .route('/:roomid/description')
+  // .route('/:roomid/description/ssr')
   // .get((req, res, next) => {
-  //   console.log('REQ PARAMS', req.params.roomId)
-  //   db.findOne(1111)
-  //     .then(doc => {console.log('DOC', doc)
-  //       res.status(200).json(doc)})
+  //   db.DescriptionModel.find({_id:id})
+  //   console.log(data)
+  //     .then(data => res.send(renderToString(<AppDescription roomId={req.params.roomid} description={data}/>)))
   //     .catch(err => {
   //       console.log('Error retrieving description for room ', req.params.roomid, ' from database');
   //       res.sendStatus(404);
@@ -75,3 +78,5 @@ module.exports = router;
   // .options((req, res) => {
   //   res.sendStatus(200);
   // });
+
+module.exports = router;
