77b82516cb939604ade5a9ee76aad5b29a3e756f
j-ornelas <johnnyoh1988@gmail.com>
2018-03-28 12:44:17 -0700

    No longer passes props to main compoenent.

--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppDescription from './AppDescription.jsx';
 
-let roomId = window.location.href.split('/')[4];
-ReactDOM.hydrate(<AppDescription roomId={roomId}></AppDescription>, document.getElementById('description-service'));
+
+// let roomId = window.location.href.split('/')[4];
+ReactDOM.hydrate(<AppDescription></AppDescription>, document.getElementById('description-service'));
