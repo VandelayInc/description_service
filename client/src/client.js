7bd989abf7d5136d44f4fde1f6e499d88dfb234c
j-ornelas <johnnyoh1988@gmail.com>
2018-03-28 12:39:08 -0700

    Adds files for SSR

---
+++ client/src/client.js
@@ -0,0 +1,4 @@
+import Description from './js/index.jsx';
+import '../css/style.scss';
+
+window.Description = Description;
---
+++ client/src/server.js
@@ -0,0 +1,3 @@
+import Description from './js/AppDescription.jsx';
+
+export default Description;
